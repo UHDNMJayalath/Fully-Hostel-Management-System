2ed856667d2c935ce909a02079621851bf8ac5b1
UHDNMJayalath <nishakamahesh2020@gmail.com>
2025-07-17 03:13:02 +0530

    Modified BursarDashboardController

--- HostelManagementSystem/src/main/java/com/hostelManagementSystem/HostelManagementSystem/controller/SubWardenDashboardController.java
+++
@@ -1,4 +0,0 @@
-package com.hostelManagementSystem.HostelManagementSystem.controller;
-
-public class SubWardenDashboardController {
-}
---
+++ HostelManagementSystem/src/main/java/com/hostelManagementSystem/HostelManagementSystem/entity/UserRoles.java
@@ -0,0 +1,4 @@
+package com.hostelManagementSystem.HostelManagementSystem.entity;
+
+public class UserRoles {
+}
